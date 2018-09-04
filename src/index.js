a05ecc8865471fbb13c22df94436ad190a7be1bd
yindi <17744559519@163.com>
2018-09-04 15:57:46 +0800

    提交文件

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import icon from './webpack.svg';
 
 function component() {
   var element = document.createElement('div');
+  var webpackImage = new Image();
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+  element.classList.add('hello');
+  webpackImage.src = icon;
+  element.appendChild(webpackImage);
+  
   return element;
 }
 
