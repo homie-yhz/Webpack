667eed0fb42ed2a504d86f4a5e407c2c300e298c
Edsion <17744559519@163.com>
2018-11-16 13:26:55 +0800

    '删除两个无用配置文件'

--- webpack.config.js
+++ webpack.config.js
@@ -55,10 +55,10 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
+        use: ['file-loader'] 
       }
     ]
-  },
+  }, 
   plugins: [
     new CleanWebpackPlugin('./dist/*.*'),
     new HtmlWebpackPlugin({ template: './index.html' }),
